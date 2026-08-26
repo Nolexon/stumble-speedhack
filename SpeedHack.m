7e1b07df11a664a9b5251616367ddda8dcfd0f72
Nolexon <142647897+Nolexon@users.noreply.github.com>
2026-08-26 09:14:09 -0400

    Update SpeedHack.m

--- SpeedHack.m
+++ SpeedHack.m
@@ -1,4 +1,4 @@
-// SpeedHack.m – Stumble Guys Speed + No Cooldown (fishhook + inline hook)
+// SpeedHack.m – Stumble Guys Speed + No Cooldown (GUI fixed)
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import <dlfcn.h>
@@ -20,10 +20,13 @@ static void *trampoline_update_cooldown = NULL;
 - (void)sliderValueChanged:(UISlider *)sender {
     speedMultiplier = sender.value;
     if (self.valueLabel) {
-        self.valueLabel.text = [NSString stringWithFormat:@"Speed: %.1fx", speedMultiplier];
+        self.valueLabel.text = [NSString stringWithFormat:@"%.1fx", speedMultiplier];
     }
+    NSLog(@"[SpeedHack] Slider changed: %.2f", speedMultiplier);
 }
 @end
+
+static SliderController *g_sliderController = nil;
 
 void hooked_set_timeScale(float value) {
     if (orig_set_timeScale) orig_set_timeScale(value * speedMultiplier);
@@ -65,86 +68,56 @@ void install_inline_hook(void *target, void *hook, void **orig, void **tramp) {
     *orig = target;
 }
 
-static UIWindow *menuWindow = nil;
-
 void showSliderMenu(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"[SpeedHack] showSliderMenu called");
         if (menuWindow) return;
-        
+
         CGRect screen = [UIScreen mainScreen].bounds;
         menuWindow = [[UIWindow alloc] initWithFrame:screen];
         menuWindow.windowLevel = UIWindowLevelAlert + 100;
         menuWindow.backgroundColor = [UIColor clearColor];
         menuWindow.hidden = NO;
-        menuWindow.userInteractionEnabled = YES;
 
         UIViewController *vc = [[UIViewController alloc] init];
-        vc.view.backgroundColor = [UIColor clearColor];
-        vc.view.frame = screen;
-        
-        // Centered popup container
-        UIView *popupContainer = [[UIView alloc] initWithFrame:CGRectMake(screen.size.width / 2 - 150, screen.size.height / 2 - 100, 300, 200)];
-        popupContainer.backgroundColor = [UIColor colorWithRed:0.15 green:0.15 blue:0.2 alpha:0.98];
-        popupContainer.layer.cornerRadius = 20;
-        popupContainer.layer.borderColor = [UIColor colorWithRed:0.3 green:0.6 blue:1.0 alpha:0.8].CGColor;
-        popupContainer.layer.borderWidth = 2;
-        popupContainer.layer.shadowColor = [UIColor blackColor].CGColor;
-        popupContainer.layer.shadowOpacity = 0.9;
-        popupContainer.layer.shadowOffset = CGSizeMake(0, 10);
-        popupContainer.layer.shadowRadius = 20;
-        popupContainer.layer.masksToBounds = NO;
-        popupContainer.transform = CGAffineTransformMakeScale(0.1, 0.1);
-        popupContainer.alpha = 0;
-        [vc.view addSubview:popupContainer];
+        UIView *bg = [[UIView alloc] initWithFrame:CGRectMake(20, 100, screen.size.width-40, 120)];
+        bg.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.1 alpha:0.85];
+        bg.layer.cornerRadius = 12;
+        [vc.view addSubview:bg];
 
-        UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, 15, 270, 35)];
-        titleLabel.text = @"⚡ Speed Hack";
-        titleLabel.textColor = [UIColor colorWithRed:0.2 green:0.8 blue:1.0 alpha:1.0];
-        titleLabel.font = [UIFont boldSystemFontOfSize:22];
-        titleLabel.textAlignment = NSTextAlignmentCenter;
-        [popupContainer addSubview:titleLabel];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 15, 200, 30)];
+        label.text = @"Speed Multiplier";
+        label.textColor = [UIColor whiteColor];
+        label.font = [UIFont boldSystemFontOfSize:16];
+        [bg addSubview:label];
 
-        UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(20, 60, 260, 30)];
+        UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(20, 50, bg.bounds.size.width-40, 30)];
         slider.minimumValue = 0.1f;
         slider.maximumValue = 20.0f;
         slider.value = speedMultiplier;
-        slider.minimumTrackTintColor = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
-        slider.maximumTrackTintColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.5];
-        slider.thumbTintColor = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
-        [popupContainer addSubview:slider];
+        [bg addSubview:slider];
 
-        UILabel *valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, 100, 270, 30)];
-        valueLabel.text = [NSString stringWithFormat:@"Speed: %.1fx", speedMultiplier];
-        valueLabel.textColor = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
-        valueLabel.font = [UIFont boldSystemFontOfSize:18];
-        valueLabel.textAlignment = NSTextAlignmentCenter;
-        [popupContainer addSubview:valueLabel];
+        UILabel *valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 85, 200, 25)];
+        valueLabel.text = [NSString stringWithFormat:@"%.1fx", speedMultiplier];
+        valueLabel.textColor = [UIColor greenColor];
+        valueLabel.font = [UIFont systemFontOfSize:14];
+        [bg addSubview:valueLabel];
 
-        UILabel *infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, 135, 270, 50)];
-        infoLabel.text = @"Slide to adjust speed multiplier";
-        infoLabel.textColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:0.7];
-        infoLabel.font = [UIFont systemFontOfSize:12];
-        infoLabel.textAlignment = NSTextAlignmentCenter;
-        infoLabel.numberOfLines = 2;
-        [popupContainer addSubview:infoLabel];
-
-        SliderController *controller = [[SliderController alloc] init];
-        controller.slider = slider;
-        controller.valueLabel = valueLabel;
-        [slider addTarget:controller action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+        g_sliderController = [[SliderController alloc] init];
+        g_sliderController.slider = slider;
+        g_sliderController.valueLabel = valueLabel;
+        [slider addTarget:g_sliderController action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
 
         menuWindow.rootViewController = vc;
-        
-        // Spring pop-up animation
-        [UIView animateWithDuration:0.6 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:0.8 options:UIViewAnimationOptionCurveEaseOut animations:^{
-            popupContainer.transform = CGAffineTransformMakeScale(1.0, 1.0);
-            popupContainer.alpha = 1.0;
-        } completion:nil];
+        [menuWindow makeKeyAndVisible];
+
+        NSLog(@"[SpeedHack] Menu window displayed");
     });
 }
 
 __attribute__((constructor))
 static void init(void) {
+    NSLog(@"[SpeedHack] Constructor called");
     struct rebinding rebindings[] = {
         {"_UnityEngine_Time_set_timeScale", (void *)hooked_set_timeScale, (void **)&orig_set_timeScale},
         {"UnityEngine_Time_set_timeScale", (void *)hooked_set_timeScale, (void **)&orig_set_timeScale}
@@ -157,7 +130,7 @@ static void init(void) {
         install_inline_hook(cooldown_addr, (void *)hooked_update_cooldown, &orig_update_cooldown, &trampoline_update_cooldown);
     }
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         showSliderMenu();
     });
 }

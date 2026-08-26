d1e6b17b7e9ad09935688e160e28be91abdf1623
Nolexon <142647897+Nolexon@users.noreply.github.com>
2026-08-26 08:56:02 -0400

    Update SpeedHack.m

--- SpeedHack.m
+++ SpeedHack.m
@@ -1,4 +1,4 @@
-// SpeedHack.c – Stumble Guys Speed + No Cooldown (fishhook + manual ARM64 hook)
+// SpeedHack.m – Stumble Guys Speed + No Cooldown (fishhook + inline hook)
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import <dlfcn.h>
@@ -6,21 +6,30 @@
 #include "fishhook.h"
 
 static float speedMultiplier = 5.0f;
-
-// Original Unity timeScale setter
 static void (*orig_set_timeScale)(float);
-
-// No cooldown hook – UpdateCooldown at RVA 0x42C967C
 static void *orig_update_cooldown = NULL;
 static void *trampoline_update_cooldown = NULL;
+
+@interface SliderController : NSObject
+@property (nonatomic, strong) UISlider *slider;
+@property (nonatomic, strong) UILabel *valueLabel;
+- (void)sliderValueChanged:(UISlider *)sender;
+@end
+
+@implementation SliderController
+- (void)sliderValueChanged:(UISlider *)sender {
+    speedMultiplier = sender.value;
+    if (self.valueLabel) {
+        self.valueLabel.text = [NSString stringWithFormat:@"%.1fx", speedMultiplier];
+    }
+}
+@end
 
 void hooked_set_timeScale(float value) {
     if (orig_set_timeScale) orig_set_timeScale(value * speedMultiplier);
 }
 
-// ARM64 trampoline: saves first 4 instructions, then branches back
 void hooked_update_cooldown(void *instance, uint64_t datetime, bool forceReset) {
-    // Force forceReset = true
     ((void (*)(void *, uint64_t, bool))trampoline_update_cooldown)(instance, datetime, true);
 }
 
@@ -33,18 +42,11 @@ uintptr_t get_unity_framework_base(void) {
     return 0;
 }
 
-// Simple ARM64 inline hook: patches first 4 instructions with a branch to hook
 void install_inline_hook(void *target, void *hook, void **orig, void **tramp) {
     if (!target || !hook) return;
-
-    // Allocate trampoline memory
     *tramp = mmap(NULL, 64, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_PRIVATE, -1, 0);
     if (*tramp == MAP_FAILED) return;
-
-    // Save original first 4 instructions (16 bytes)
     memcpy(*tramp, target, 16);
-
-    // Add branch back after saved instructions
     uint8_t *tramp_code = (uint8_t *)*tramp;
     uintptr_t back_addr = (uintptr_t)target + 16;
     uintptr_t tramp_end = (uintptr_t)tramp_code + 16;
@@ -52,23 +54,17 @@ void install_inline_hook(void *target, void *hook, void **orig, void **tramp) {
     uint32_t branch_back = 0x14000000 | (offset & 0x03FFFFFF);
     *(uint32_t *)(tramp_code + 16) = branch_back;
 
-    // Patch target with branch to hook
     uintptr_t target_addr = (uintptr_t)target;
     uintptr_t hook_addr = (uintptr_t)hook;
     int64_t hook_offset = (hook_addr - target_addr) / 4;
-    if (hook_offset < -0x02000000 || hook_offset > 0x01FFFFFF) return; // out of range
-
+    if (hook_offset < -0x02000000 || hook_offset > 0x01FFFFFF) return;
     uint32_t branch_inst = 0x14000000 | (hook_offset & 0x03FFFFFF);
-
-    // Change memory protection
     vm_protect(mach_task_self(), (vm_address_t)target, 16, 0, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
     *(uint32_t *)target = branch_inst;
     vm_protect(mach_task_self(), (vm_address_t)target, 16, 0, VM_PROT_READ | VM_PROT_EXECUTE);
-
     *orig = target;
 }
 
-// Slider UI
 static UIWindow *menuWindow = nil;
 
 void showSliderMenu(void) {
@@ -96,20 +92,18 @@ void showSliderMenu(void) {
         slider.minimumValue = 0.1f;
         slider.maximumValue = 20.0f;
         slider.value = speedMultiplier;
-        [slider addTarget:^{ speedMultiplier = slider.value; } action:@selector(invoke) forControlEvents:UIControlEventValueChanged];
         [bg addSubview:slider];
 
         UILabel *valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 85, 200, 25)];
         valueLabel.text = [NSString stringWithFormat:@"%.1fx", speedMultiplier];
         valueLabel.textColor = [UIColor greenColor];
         valueLabel.font = [UIFont systemFontOfSize:14];
-        valueLabel.tag = 99;
         [bg addSubview:valueLabel];
 
-        [slider addTarget:^{
-            UILabel *vl = (UILabel *)[bg viewWithTag:99];
-            if (vl) vl.text = [NSString stringWithFormat:@"%.1fx", speedMultiplier];
-        } action:@selector(invoke) forControlEvents:UIControlEventValueChanged];
+        SliderController *controller = [[SliderController alloc] init];
+        controller.slider = slider;
+        controller.valueLabel = valueLabel;
+        [slider addTarget:controller action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
 
         menuWindow.rootViewController = vc;
         menuWindow.userInteractionEnabled = YES;
@@ -118,14 +112,12 @@ void showSliderMenu(void) {
 
 __attribute__((constructor))
 static void init(void) {
-    // Hook Unity timeScale using fishhook
     struct rebinding rebindings[] = {
         {"_UnityEngine_Time_set_timeScale", (void *)hooked_set_timeScale, (void **)&orig_set_timeScale},
         {"UnityEngine_Time_set_timeScale", (void *)hooked_set_timeScale, (void **)&orig_set_timeScale}
     };
     rebind_symbols(rebindings, 2);
 
-    // Install inline hook for UpdateCooldown
     uintptr_t base = get_unity_framework_base();
     if (base) {
         void *cooldown_addr = (void *)(base + 0x42C967C);

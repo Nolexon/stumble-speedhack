968949706122053ecf438f2604b44332ba4637b8
Nolexon <142647897+Nolexon@users.noreply.github.com>
2026-08-26 11:58:53 -0400

    Update SpeedHack.m

--- SpeedHack.m
+++ SpeedHack.m
@@ -1,22 +1,40 @@
-// SpeedHack.m – Stumble Guys Speed 15x + No Cooldown (timer enforced)
+// SpeedHack.m – Stumble Guys Speed 15x + No Cooldown with all fallbacks
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import <dlfcn.h>
 #import <mach-o/dyld.h>
 #include "fishhook.h"
 
-static float targetSpeed = 15.0f;
+static float speedMultiplier = 15.0f;
 static void (*orig_set_timeScale)(float) = NULL;
 static void *orig_update_cooldown = NULL;
 static void *trampoline_update_cooldown = NULL;
 
+// Status flags for popup
+static BOOL speedHookOK = NO;
+static BOOL speedDlsymOK = NO;
+static BOOL speedTimerOK = NO;
+static BOOL cooldownHookOK = NO;
+
+// ---------------------------------------------------------------------
+// Speed hook functions
+// ---------------------------------------------------------------------
 void hooked_set_timeScale(float value) {
-    // Force every setter call to targetSpeed
     if (orig_set_timeScale) {
-        orig_set_timeScale(targetSpeed);
+        orig_set_timeScale(value * speedMultiplier);
     }
 }
 
+// Timer callback that enforces speed every 0.2 seconds
+void enforceSpeedTimer(CFRunLoopTimerRef timer, void *info) {
+    if (orig_set_timeScale) {
+        orig_set_timeScale(speedMultiplier);
+    }
+}
+
+// ---------------------------------------------------------------------
+// No cooldown hook
+// ---------------------------------------------------------------------
 void hooked_update_cooldown(void *instance, uint64_t datetime, bool forceReset) {
     ((void (*)(void *, uint64_t, bool))trampoline_update_cooldown)(instance, datetime, true);
 }
@@ -53,45 +71,93 @@ void install_inline_hook(void *target, void *hook, void **orig, void **tramp) {
     *orig = target;
 }
 
-// Timer callback that enforces speed every 0.2 seconds
-void enforceSpeedTimer(CFRunLoopTimerRef timer, void *info) {
-    if (orig_set_timeScale) {
-        orig_set_timeScale(targetSpeed);
-    }
+// ---------------------------------------------------------------------
+// Popup
+// ---------------------------------------------------------------------
+void showStatusPopup(NSString *message) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindow *keyWindow = nil;
+        for (UIWindow *window in [UIApplication sharedApplication].windows) {
+            if (window.isKeyWindow) {
+                keyWindow = window;
+                break;
+            }
+        }
+        UIViewController *rootVC = keyWindow ? keyWindow.rootViewController : nil;
+        if (rootVC) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"SpeedHack" message:message preferredStyle:UIAlertControllerStyleAlert];
+            [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+            [rootVC presentViewController:alert animated:YES completion:nil];
+        } else {
+            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, 240, 100)];
+            label.text = message;
+            label.numberOfLines = 0;
+            label.backgroundColor = [UIColor blackColor];
+            label.textColor = [UIColor greenColor];
+            label.textAlignment = NSTextAlignmentCenter;
+            label.layer.cornerRadius = 8;
+            label.clipsToBounds = YES;
+            [keyWindow addSubview:label];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                [label removeFromSuperview];
+            });
+        }
+    });
 }
 
+// ---------------------------------------------------------------------
+// Constructor
+// ---------------------------------------------------------------------
 __attribute__((constructor))
 static void init(void) {
-    // Hook Unity timeScale setter using fishhook
+    // 1) fishhook for timeScale setter
     struct rebinding rebindings[] = {
         {"_UnityEngine_Time_set_timeScale", (void *)hooked_set_timeScale, (void **)&orig_set_timeScale},
         {"UnityEngine_Time_set_timeScale", (void *)hooked_set_timeScale, (void **)&orig_set_timeScale}
     };
     rebind_symbols(rebindings, 2);
+    if (orig_set_timeScale) {
+        speedHookOK = YES;
+    }
 
-    // If fishhook failed, try dlsym directly
+    // 2) dlsym direct fallback
     if (!orig_set_timeScale) {
         void *sym = dlsym(RTLD_DEFAULT, "_UnityEngine_Time_set_timeScale");
         if (!sym) sym = dlsym(RTLD_DEFAULT, "UnityEngine_Time_set_timeScale");
         if (sym) {
             orig_set_timeScale = (void (*)(float))sym;
-            // Directly call it once
-            orig_set_timeScale(targetSpeed);
+            orig_set_timeScale(speedMultiplier);
+            speedDlsymOK = YES;
         }
     }
 
-    // Start a timer to keep speed enforced
-    CFRunLoopTimerRef speedTimer = CFRunLoopTimerCreate(NULL, CFAbsoluteTimeGetCurrent(), 0.2, 0, 0, &enforceSpeedTimer, NULL);
-    if (speedTimer) {
-        CFRunLoopAddTimer(CFRunLoopGetMain(), speedTimer, kCFRunLoopCommonModes);
+    // 3) Timer enforcement fallback
+    if (orig_set_timeScale) {
+        CFRunLoopTimerRef speedTimer = CFRunLoopTimerCreate(NULL, CFAbsoluteTimeGetCurrent(), 0.2, 0, 0, &enforceSpeedTimer, NULL);
+        if (speedTimer) {
+            CFRunLoopAddTimer(CFRunLoopGetMain(), speedTimer, kCFRunLoopCommonModes);
+            speedTimerOK = YES;
+        }
     }
 
-    // Hook UpdateCooldown
+    // 4) No cooldown inline hook
     uintptr_t base = get_unity_framework_base();
     if (base) {
         void *cooldown_addr = (void *)(base + 0x42C967C);
         install_inline_hook(cooldown_addr, (void *)hooked_update_cooldown, &orig_update_cooldown, &trampoline_update_cooldown);
+        if (trampoline_update_cooldown) {
+            cooldownHookOK = YES;
+        }
     }
-}
 
-//KHI MADE THIS//
+    // 5) Popup with status
+    NSString *status = [NSString stringWithFormat:
+        @"Speed hook: %@\nDlsym fallback: %@\nTimer: %@\nCooldown hook: %@",
+        speedHookOK ? @"YES" : @"NO",
+        speedDlsymOK ? @"YES" : @"NO",
+        speedTimerOK ? @"YES" : @"NO",
+        cooldownHookOK ? @"YES" : @"NO"];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        showStatusPopup(status);
+    });
+}
